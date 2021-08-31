2d14181bc0c1a527cded8a18b5b292a99fa72b90
Amir Golparvar <golparva>
2021-08-31 18:35:41 +0200

    Database done...

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const userRepo = require('./repositories/users')
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true}));
@@ -40,8 +40,17 @@ app.get('/', (req, res) => {
 //   res.send('Account created');
 // });
 
-app.post('/', (req, res) => {
-  console.log(req.body);
+app.post('/', async (req, res) => {
+  const { email, password, passwordConfirmation } = req.body;
+
+  const existingUser = await userRepo.getOneBy({ email });
+  if (existingUser) {
+    return res.send('Email in use')
+  }
+
+  if (password!==passwordConfirmation) {
+    return res.send('Passwords must match!')
+  }
   res.send('Account created');
 });
 
